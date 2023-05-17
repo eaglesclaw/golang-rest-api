4557629d53ebddc8047c9f4581228cf750e335ed
eaglesclaw <sduzen95@gmail.com>
2023-05-18 00:12:47 +0300

    update

--- httpserver/cmd/gorest/main.go
+++ httpserver/cmd/gorest/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"Youtube-Projects/golang-rest-api/httpserver/pkg/ui"
-
 	"github.com/eaglesclaw/golang-rest-api/pkg/api"
 	"github.com/eaglesclaw/golang-rest-api/pkg/database"
 	"github.com/eaglesclaw/golang-rest-api/pkg/ui"
@@ -14,7 +12,6 @@ import (
 
 func main(){
 	fmt.Println("......main")
-
 
 	api.Api()
 	database.Database()
--- httpserver/pkg/api/api.go
+++ httpserver/pkg/api/api.go
@@ -1,7 +1,25 @@
 package api
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func Api(){
 	fmt.Println("Api")
+
+	http.HandleFunc("/",index)
+	http.HandleFunc("/post",post)
+
+	http.ListenAndServe(":8080", nil)
 }
+
+func index(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("index page!!!!"))
+}
+
+func post(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("post page!!!!"))
+}
+
+
